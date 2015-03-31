085f37faf3e174aa54baf0d144b8d878d00ed0cc
Daniel Flax <dflax@yahoo.com>
2015-03-31 14:09:16 -0400

    Added Start Game button functionality with touchesBegan()

--- Invaders/StartGameScene.swift
+++ Invaders/StartGameScene.swift
@@ -18,4 +18,19 @@ class StartGameScene: SKScene {
 		addChild(startGameButton)
 	}
 
+	override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+		for touch: AnyObject in touches {
+			let touchLocation = touch.locationInNode(self)
+			let touchedNode = self.nodeAtPoint(touchLocation)
+			if(touchedNode.name == "startgame"){
+				let gameOverScene = GameScene(size: size)
+				gameOverScene.scaleMode = scaleMode
+				let transitionType = SKTransition.flipHorizontalWithDuration(1.0)
+				view?.presentScene(gameOverScene,transition: transitionType)
+			}
+		}
+	}
+
+
+
 }
